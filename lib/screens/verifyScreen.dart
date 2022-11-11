b819a0012158d7085bdbe0661c3462ec6aa7a70c
UP968909 <91615547+UP968909@users.noreply.github.com>
2022-11-11 01:33:16 +0000

    added firebase files

--- lib/screens/verifyScreen.dart
+++ lib/screens/verifyScreen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
