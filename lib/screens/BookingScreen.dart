74e2839212f6af61794e4d299916c74fc853e2d4
UP968909 <91615547+UP968909@users.noreply.github.com>
2022-11-23 17:27:08 +0000

    trying a new calendar booking view

--- lib/screens/BookingScreen.dart
+++ lib/screens/BookingScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sport_test/screens/try.dart';
 
 import 'dart:math' as math;
 
@@ -8,9 +9,9 @@ import 'calendarView.dart';
 class BookingScreen extends StatelessWidget {
   static const _actionTitles = [
     'Book a gym session',
+    'Book a gym class',
     'Book a Swimming session',
     'Book a climbing session',
-    'Book a gym class'
   ];
 
   const BookingScreen({super.key});
@@ -28,7 +29,7 @@ class BookingScreen extends StatelessWidget {
             ),
             TextButton(
               onPressed: () => Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (context) => const BookView())),
+                  MaterialPageRoute(builder: (context) => const BookingCalendarDemoApp())),
               child: const Text('Accept'),
             )
           ],
@@ -39,216 +40,68 @@ class BookingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Map> activities =
+        List.generate(4, (index) => {"id": index, "name": "Activity $index"})
+            .toList();
+
+    final List activityImages = [
+      'lib/assets/images/gym_dumbbell.png',
+      'lib/assets/images/classes_2.png',
+      'lib/assets/images/swimming.png',
+      'lib/assets/images/rock_climbing.png',
+    ];
+
+    final List activityTitles = [
+      'Gym Session',
+      'Gym classes',
+      'Swimming',
+      'Climbing'
+    ];
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.purple,
-        title: const Text('Bookings'),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      floatingActionButton: ExpandableFab(
-        distance: 112.0,
-        children: [
-          ActionButton(
-            onPressed: () => _showAction(context, 0),
-            icon: Image.asset('lib/assets/images/gym_dumbbell.png'),
-          ),
-          ActionButton(
-            onPressed: () => _showAction(context, 3),
-            icon: Image.asset('lib/assets/images/classes_2.png'),
-          ),
-          ActionButton(
-            onPressed: () => _showAction(context, 2),
-            icon: Image.asset('lib/assets/images/rock_climbing.png'),
-          ),
-          ActionButton(
-            onPressed: () => _showAction(context, 1),
-            icon: Image.asset('lib/assets/images/swimming.png'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-@immutable
-class ExpandableFab extends StatefulWidget {
-  const ExpandableFab({
-    super.key,
-    this.initialOpen,
-    required this.distance,
-    required this.children,
-  });
-
-  final bool? initialOpen;
-  final double distance;
-  final List<Widget> children;
-
-  @override
-  State<ExpandableFab> createState() => _ExpandableFabState();
-}
-
-class _ExpandableFabState extends State<ExpandableFab>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _controller;
-  late final Animation<double> _expandAnimation;
-  bool _open = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _open = widget.initialOpen ?? false;
-    _controller = AnimationController(
-      value: _open ? 1.0 : 0.0,
-      duration: const Duration(milliseconds: 250),
-      vsync: this,
-    );
-    _expandAnimation = CurvedAnimation(
-      curve: Curves.fastOutSlowIn,
-      reverseCurve: Curves.easeOutQuad,
-      parent: _controller,
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _toggle() {
-    setState(() {
-      _open = !_open;
-      if (_open) {
-        _controller.forward();
-      } else {
-        _controller.reverse();
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: Stack(
-        alignment: Alignment.bottomRight,
-        clipBehavior: Clip.none,
-        children: [
-          _buildTapToCloseFab(),
-          ..._buildExpandingActionButtons(),
-          _buildTapToOpenFab(),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildTapToCloseFab() {
-    return SizedBox(
-      width: 56.0,
-      height: 56.0,
-      child: Center(
-        child: Material(
-          shape: const CircleBorder(),
-          clipBehavior: Clip.antiAlias,
-          elevation: 4.0,
-          child: InkWell(
-            onTap: _toggle,
-            child: const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Icon(
-                Icons.close,
-                color: Colors.purple,
-              ),
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.purple,
+          title: const Text('Bookings'),
+        ),
+        body: Center(
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
             ),
+            itemCount: activities.length,
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onTap: () {
+                  _showAction(context, index); // call click event
+                },
+                child: Card(
+                  color: Colors.purple,
+                  shadowColor: Colors.purpleAccent,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        activityImages[index],
+                        fit: BoxFit.cover,
+                        height: 50,
+                        width: 50,
+                        color: Colors.white,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        activityTitles[index],
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
           ),
-        ),
-      ),
-    );
-  }
-
-  List<Widget> _buildExpandingActionButtons() {
-    final children = <Widget>[];
-    final count = widget.children.length;
-    final step = 90.0 / (count - 1);
-    for (var i = 0, angleInDegrees = 0.0;
-        i < count;
-        i++, angleInDegrees += step) {
-      children.add(
-        _ExpandingActionButton(
-          directionInDegrees: angleInDegrees,
-          maxDistance: widget.distance,
-          progress: _expandAnimation,
-          child: widget.children[i],
-        ),
-      );
-    }
-    return children;
-  }
-
-  Widget _buildTapToOpenFab() {
-    return IgnorePointer(
-      ignoring: _open,
-      child: AnimatedContainer(
-        transformAlignment: Alignment.center,
-        transform: Matrix4.diagonal3Values(
-          _open ? 0.7 : 1.0,
-          _open ? 0.7 : 1.0,
-          1.0,
-        ),
-        duration: const Duration(milliseconds: 250),
-        curve: const Interval(0.0, 0.5, curve: Curves.easeOut),
-        child: AnimatedOpacity(
-          opacity: _open ? 0.0 : 1.0,
-          curve: const Interval(0.25, 1.0, curve: Curves.easeInOut),
-          duration: const Duration(milliseconds: 250),
-          child: FloatingActionButton(
-            backgroundColor: Colors.purple,
-            onPressed: _toggle,
-            child: const Icon(Icons.add, color: Colors.white),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-@immutable
-class _ExpandingActionButton extends StatelessWidget {
-  const _ExpandingActionButton({
-    required this.directionInDegrees,
-    required this.maxDistance,
-    required this.progress,
-    required this.child,
-  });
-
-  final double directionInDegrees;
-  final double maxDistance;
-  final Animation<double> progress;
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: progress,
-      builder: (context, child) {
-        final offset = Offset.fromDirection(
-          directionInDegrees * (math.pi / 180.0),
-          progress.value * maxDistance,
-        );
-        return Positioned(
-          right: 4.0 + offset.dx,
-          bottom: 4.0 + offset.dy,
-          child: Transform.rotate(
-            angle: (1.0 - progress.value) * math.pi / 2,
-            child: child!,
-          ),
-        );
-      },
-      child: FadeTransition(
-        opacity: progress,
-        child: child,
-      ),
-    );
+        ));
   }
 }
 
@@ -279,25 +132,3 @@ class ActionButton extends StatelessWidget {
     );
   }
 }
-
-// @immutable
-// class FakeItem extends StatelessWidget {
-//   const FakeItem({
-//     super.key,
-//     required this.isBig,
-//   });
-
-//   final bool isBig;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
-//       height: isBig ? 128.0 : 36.0,
-//       decoration: BoxDecoration(
-//         borderRadius: const BorderRadius.all(Radius.circular(8.0)),
-//         color: Colors.grey.shade300,
-//       ),
-// //     );
-// //   }
-// // }
